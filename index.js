4ec4b345d3992a534bb1ea629230182a06c59e92
Vinely <96441768+Vinellon@users.noreply.github.com>
2024-12-27 17:48:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const target = process.env.MAP_SERVER_URL; // Например, "http://your-minecraft-server:8080"
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+  if (!target) {
+    res.statusCode = 500;
+    res.end('MAP_SERVER_URL is not set');
+    return;
+  }
+  proxy.web(req, res, { target });
+});
+
+server.listen(3000, () => {
+  console.log('Proxy server is running on http://localhost:3000');
+});
